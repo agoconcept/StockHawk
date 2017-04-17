fed3e5c367c6009f28759061d102170a1c46db45
Santiago Gallego <santiago.gallego.rodrigo@gmail.com>
2017-04-17 13:02:58 +0200

    Fix error when adding a non existing stock

--- app/src/main/java/com/udacity/stockhawk/ui/AddStockDialog.java
+++ app/src/main/java/com/udacity/stockhawk/ui/AddStockDialog.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -14,11 +15,18 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.udacity.stockhawk.R;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import timber.log.Timber;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
 
 public class AddStockDialog extends DialogFragment {
@@ -66,12 +74,58 @@ public class AddStockDialog extends DialogFragment {
     }
 
     private void addStock() {
+
         Activity parent = getActivity();
-        if (parent instanceof MainActivity) {
-            ((MainActivity) parent).addStock(stock.getText().toString());
+        String stockSymbol = stock.getText().toString();
+
+        // Check if the stock symbol is valid in another thread, because it needs
+        // to request it over the network
+        AsyncTask task = new CheckStockTask().execute(stockSymbol);
+
+        try {
+            // Wait until the request finishes
+            Boolean stockExists = (Boolean) task.get();
+            if (stockExists) {
+                if (parent instanceof MainActivity) {
+                    ((MainActivity) parent).addStock(stockSymbol);
+                }
+            } else {
+                Timber.e("Stock \"%s\" not found", stockSymbol);
+                String message = getString(R.string.error_invalid_stock_symbol, stockSymbol);
+                Toast.makeText(parent, message, Toast.LENGTH_SHORT).show();
+            }
+        } catch (InterruptedException ie) {
+            Timber.w("Operation interrupted while trying to retrieve stock");
+        } catch (ExecutionException ee) {
+            Timber.w("Operation failed while trying to retrieve stock");
+        } finally {
+            dismissAllowingStateLoss();
         }
-        dismissAllowingStateLoss();
     }
 
+    // Get stock in another thread
+    private class CheckStockTask extends AsyncTask<String, Void, Boolean> {
 
+        @Override
+        protected Boolean doInBackground(String... stocks) {
+
+            Boolean stockExists = false;
+            String stockSymbol = stocks[0];
+
+            if (stockSymbol == null || stockSymbol.length() == 0)
+                return false;
+
+            try {
+                Stock stock = YahooFinance.get(stockSymbol);
+                if (stock.getQuote().getPrice() != null) {
+                    stockExists = true;
+                }
+            } catch (IOException exception) {
+                String message = getString(R.string.error_invalid_stock_symbol, stockSymbol);
+                Timber.e(message);
+            }
+
+            return stockExists;
+        }
+    }
 }
